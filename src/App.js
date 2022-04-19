f67b55f2aa177105b9ffe485ffafe3419dff96da
oaat9309 <kite1993@naver.com>
2022-04-19 13:55:32 +0900

    feat: Components Imported (04.19)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import "./App.css";
+import Arrows from "./components/Arrows";
+import TextBox from "./components/TextBox";
+import Button from "./components/Button";
+import Modal from "./components/Modal";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <TextBox style="input" />
+      <TextBox style="output" />
+    </div>
+  );
 }
 
 export default App;
