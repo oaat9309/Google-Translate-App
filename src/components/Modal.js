66a3c2c7039bc85b1b5531be42a4f040137d1f58
oaat9309 <kite1993@naver.com>
2022-04-29 15:20:37 +0900

    feat: Modal.js setup

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,18 @@
+import { useState } from "react";
+
 const Modal = () => {
-  return <div>Modal</div>;
+  const [searchedLanguage, setSearchedLanguage] = useState("");
+  const handleChange = (e) => {
+    setSearchedLanguage(e.target.value);
+  };
+
+  return (
+    <div className="option-list">
+      <div className="search-bar">
+        <input value={searchedLanguage} onChange={handleChange} />
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
