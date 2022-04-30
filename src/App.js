0e9c99d4c6e3569c14b097384c14ccb21b315c72
oaat9309 <kite1993@naver.com>
2022-04-30 23:27:17 +0900

    feat: App.js axios setup (04.30)

--- src/App.js
+++ src/App.js
@@ -3,17 +3,41 @@ import TextBox from "./components/TextBox";
 import Button from "./components/Button";
 import Modal from "./components/Modal";
 import { useState } from "react";
+import axios from "axios";
 
 function App() {
   const [showModal, setShowModal] = useState(null);
   const [inputLanguage, setInputLanguage] = useState("English");
   const [outputLanguage, setOutputLanguage] = useState("Polish");
+  const [languages, setLanguages] = useState(null);
+
+  const getLanguages = () => {
+    const options = {
+      method: "GET",
+      url: "https://google-translate1.p.rapidapi.com/language/translate/v2/languages",
+      headers: {
+        "Accept-Encoding": "application/gzip",
+        "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+        "X-RapidAPI-Key": "4b0a967fb3msh833759461bb33cap191f9ejsna6000c498368",
+      },
+    };
+
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data);
+        setLanguages(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
 
   const handleClick = () => {
     setInputLanguage(outputLanguage);
     setOutputLanguage(inputLanguage);
   };
-  console.log("showModal", showModal);
+
   return (
     <div className="app">
       {!showModal && (
@@ -29,7 +53,7 @@ function App() {
           <TextBox selectedLanguage={outputLanguage} style="output" />
         </>
       )}
-      {showModal && <Modal />}
+      {showModal && <Modal setShowModal={setShowModal} />}
     </div>
   );
 }
