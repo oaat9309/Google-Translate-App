47b73154297e2dec4145932f813d8fd6723f41d9
oaat9309 <kite1993@naver.com>
2022-04-30 23:26:37 +0900

    feat:Modal.js setup (04.30)

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const Modal = () => {
+const Modal = ({ setShowModal }) => {
   const [searchedLanguage, setSearchedLanguage] = useState("");
   const handleChange = (e) => {
     setSearchedLanguage(e.target.value);
@@ -10,6 +10,23 @@ const Modal = () => {
     <div className="option-list">
       <div className="search-bar">
         <input value={searchedLanguage} onChange={handleChange} />
+        <div
+          className="close-button"
+          onClick={() => {
+            setShowModal(null);
+          }}
+        >
+          <svg
+            focusable="false"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+          </svg>
+        </div>
+      </div>
+      <div className="option-container">
+        <ul></ul>
       </div>
     </div>
   );
